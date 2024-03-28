7d8f6ce1dc15714ab47da48e53d6bae2022704b1
1Sarcastic <ziadbahnass@gmail.com>
2024-03-28 12:46:41 +0100

    Login function added in "views.py" as well as a "still dont have an account" function

--- Drive_Ferm/Drive/views.py
+++ Drive_Ferm/Drive/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
@@ -24,7 +26,7 @@ import email
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages, auth
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse, reverse_lazy
@@ -87,3 +89,26 @@ def profile_view(request):
     print(profile.profile_image.url)
     context = {'profile': profile}
     return render(request, 'client-view.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')  # Assuming 'home' is the name of your homepage URL pattern
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('login')  # Redirect to login page in case of invalid credentials
+
+    return render(request, 'login.html')
+
+# views.py
+
+from django.shortcuts import render, redirect
+
+def signup(request):
+    return render(request, 'signup.html')
